da110e3eed97623fb2ace0d5b65cc12d6c5fafa0
Trần Kim Thắng <111619268+KeyT9999@users.noreply.github.com>
2026-02-05 15:01:01 +0700

    vv

--- RealtimeAuction.Api/obj/Debug/net10.0/RealtimeAuction.Api.AssemblyInfo.cs
+++ RealtimeAuction.Api/obj/Debug/net10.0/RealtimeAuction.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RealtimeAuction.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22ccf7892879020163a4425e52ad1e5f85021695")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6bbd599d2d0ce1f668ad44b9a200bd55d781792e")]
 [assembly: System.Reflection.AssemblyProductAttribute("RealtimeAuction.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RealtimeAuction.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
